99eced6258d5187bfbc9daa113bb2b7265cc7533
beomseokwee <wijang777@naver.com>
2023-09-25 15:02:58 +0900

    초기 세팅

---
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -0,0 +1,29 @@
+package com.quiz.lesson03.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson03.domain.RealEstate;
+import com.quiz.lesson03.mapper.RealEstateMapper;
+
+@Service
+public class RealEstateBO {
+
+	@Autowired
+	private RealEstateMapper realEstateMapper;
+	
+	// input: id(정수)
+	// output: RealEstate
+	public RealEstate getRealEstateById(int id) {
+		return realEstateMapper.selectRealEstateById(id);
+	}
+	
+	// input: rentPrice
+	// output: List<RealEstate>
+	public List<RealEstate> getRealEstateListByRentPrice(int rentPrice) {
+		//return null;
+		return realEstateMapper.selectRealEstateListByRentPrice(rentPrice);
+	}
+}
